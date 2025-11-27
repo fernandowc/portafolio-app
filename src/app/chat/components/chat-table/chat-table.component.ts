53457dae9f5b4f6d6c48d9d903c3277632329f5f
Luis Fernando Vidal <lvidaldev@gmail.com>
2025-11-27 02:40:14 -0500

    get datos rest en tabla

--- src/app/chat/components/chat-table/chat-table.component.ts
+++ src/app/chat/components/chat-table/chat-table.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChatRoomService, ChatRoom } from '../../service/chat-room.service';
 
 @Component({
   selector: 'chat-table',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './chat-table.component.html',
   styles: ``
 })
-export class ChatTableComponent {
+export class ChatTableComponent implements OnInit {
+  chatRooms: ChatRoom[] = [];
 
+  constructor(private chatRoomService: ChatRoomService) {}
+
+  ngOnInit() {
+    this.chatRoomService.getChatRooms().subscribe({
+      next: (rooms) => this.chatRooms = rooms,
+      error: (err) => console.error('Error cargando chat rooms', err)
+    });
+  }
 }
